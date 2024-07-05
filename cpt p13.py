cb717e11e7fbafb90ea5d72d3c0528cc87c9f9f1
harsha0736 <davuluriharshavardhansai@gmail.com>
2024-07-05 12:28:11 +0530

    13th

--- cpt p13.py
+++ cpt p13.py
@@ -1,2 +1,16 @@
-def search_target(nums,target):
-    
+def searchInsert(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right=mid-1
+    return left
+
+nums=[1,3,5,6]
+target=6
+result=searchInsert(nums, target)
+print(result)
